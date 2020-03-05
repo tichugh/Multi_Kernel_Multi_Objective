19ad30f032f184db94a35fdcee05afbf6c4a05ba
Tinkle Chugh <t.chugh@exeter.ac.uk>
2020-03-05 10:12:19 +0000

    fast calculation of inverse

--- Support_files/hyper_parameter_bi_objective_multi_kernel.m
+++ Support_files/hyper_parameter_bi_objective_multi_kernel.m
@@ -6,11 +6,13 @@ function f = hyper_parameter_bi_objective_multi_kernel(par,X,Y,kernels,CS_kernel
     K = K + 1e-8 * eye(N,N); % N x N matrix
     TT = chol(K,'lower');
     K_inv = inv_chol(TT);    
+    
+    det_K = prod(diag(TT).^2);
 
     f_temp = Y'*K_inv*Y;   
-    if det(K)==0
+    if det_K==0
         f = [0.5*f_temp,0.5*log(1e-100)];
     else
-        f = [0.5*f_temp,0.5*log(det(TT))];
+        f = [0.5*f_temp,0.5*log(det_K)];
     end
 end
